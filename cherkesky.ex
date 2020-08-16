72e5929e901f48840df1a6327f46253beabe148b
Guy Cherkesky <guy@cherkesky.com>
2020-08-15 19:41:56 -0500

    mapping as fun param

--- cherkesky.ex
+++ cherkesky.ex
@@ -1,8 +1,14 @@
-defmodule Cherkesky do
+defmodule Mod do
   def hello(stuff) do
     "Hello "<>stuff
   end
   def read_file(filename) do
     File.read(filename)
+  end
+end
+
+defmodule Guy do
+  def mapping(%{"key" => value}) do
+    IO.puts(value)
   end
 end
